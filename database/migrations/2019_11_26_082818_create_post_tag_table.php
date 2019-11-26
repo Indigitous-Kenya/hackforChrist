3d13e6bc5577e9d6718ebe680977d73d601d46de
techtenant <bnyilima@gmail.com>
2019-11-26 11:32:01 +0300

    Resolved M:M relationship between Posts and tags

---
+++ database/migrations/2019_11_26_082818_create_post_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            //$table->bigIncrements('id');
+            $table->bigInteger('post_id');
+            $table->bigInteger('tag_id');
+            $table->primary(['post_id', 'tag_id']);
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
