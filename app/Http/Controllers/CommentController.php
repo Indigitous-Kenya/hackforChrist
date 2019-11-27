8fb3469fa342ffa22496067ffb488a75bed37cf7
techtenant <bnyilima@gmail.com>
2019-11-27 17:25:20 +0300

    Comment controller customisation

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\model\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function index()
+    {
+        $comments = Comment::with(['author', 'post'])->paginate();
+        return view('')->withComments($comments);
+    }
+
+    public function show($id)
+    {
+        return view('')->withComments(Comment::find($id));
+    }
 }
