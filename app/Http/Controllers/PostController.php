3ead2f3753445e0b72ee6b105ff26d6de1dc256c
techtenant <bnyilima@gmail.com>
2019-11-27 17:59:27 +0300

    Comments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // if ($request->has('post_tags')) {
+        //     foreach($request->input('post_tags') as $id)
+        //     {
+        //         DB::table('post_tag')->insert(
+        //             [
+        //                 'post_id' => $post->id,
+        //                 'tag_id' => $id,
+        //             ]
+        //         );
+        //     }
+        //     # code...
+        // }
+    }
 }
