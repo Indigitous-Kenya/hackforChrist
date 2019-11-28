0dfde0ce61e14177d1933361f018fe774062eced
techtenant <bnyilima@gmail.com>
2019-11-28 14:49:30 +0300

    Register controller customisation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
