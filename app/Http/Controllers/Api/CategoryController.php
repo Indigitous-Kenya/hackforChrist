801d228638f7373bb41cee4fb85e3dc02bd14111
techtenant <bnyilima@gmail.com>
2019-11-28 10:20:50 +0300

    API return data

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoriesResource;
+use App\model\Category;
 
 class CategoryController extends Controller
 {
@@ -16,7 +17,8 @@ class CategoryController extends Controller
     }
     public function index()
     {
-        return new CategoriesResource($this->categoryMaster->index()) ;
+        $categories = new Category();
+        return new CategoriesResource($categories) ;
     }
 
     public function show($id)
